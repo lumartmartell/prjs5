b821c54c2ddda8b5551f773205936436874a6952
lumart <qxulw@mail.ru>
2025-04-05 15:44:11 +0300

    1

---
+++ script.js
@@ -0,0 +1,32 @@
+// 'use strict'
+
+// let user = {
+//     name: 'nana',
+//     age: 20,
+//     sayHello(){
+//         alert('Hello my name is' + this.name )
+//     }
+// };
+
+// user.sayHello();
+
+// // console.log(user)
+// // console.log(user.age) //20
+
+// user.surname = 'nana'
+// // console.log(user)
+
+// // for(let key in user){
+// //     console.log(key);
+// //     console.log(user[key]);
+// // }
+
+// let arr = new Array();
+// arr = ['Бабуинни капучинни', 'тралалело тралала'];
+// console.log(arr[0]);
+// console.log(arr.length);
+
+// // arr = ['капучинни', 'тралала', 1, 8]
+
+// arr.push('бри бри брикус дикус де брикус?');
+// console,log(arr);
